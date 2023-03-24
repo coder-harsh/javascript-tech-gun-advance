1aa14bef77cb67305cbc0aeda65375040cdf2b99
Harsh <harshj7.net@gmail.com>
2023-03-24 14:42:52 +0530

    Add all 84 template literal till line no 6

---
+++ js/84-template-literals.js
@@ -0,0 +1,6 @@
+let head = document.getElementById("head");
+let h1 = head.innerHTML;
+console.log(h1); //Template literal
+
+console.log(`text is:${h1}`);  //text is:Template literal
+console.log(`Sum is:${2 + 3}`);  //Sum is:5 we can do mathematical calculation
