ba0da25123b52cbd9c9a78de382f748e6836d6ed
Harsh <harshj7.net@gmail.com>
2023-03-25 21:13:54 +0530

    add all 88 oopps till line no 64

---
+++ js/88-object-oriented-prog.js
@@ -0,0 +1,64 @@
+/*
+class and object or oops hume code repeat se bachta h. 
+abhi tak hum jo kar rahe the wo procedural tha... 
+es5 tk js m oops concept alag tarh se implement kiya jata tha.
+es6 m class se oops concept implement kiya jata h.
+js m es5 ko puri tarah remove nhi kiya gaya h.
+es5 m oops concept kaise implement kiya jata tha interview m puchh sakta h.
+
+
+
+
+3 most imp words to understand
+
+object 
+class
+inheritance
+
+
+object: things or vastu
+ex: vishwjeet, hodacity car.
+
+
+har object have 2 things.
+
+1. feature/properties: full name,height, age etc. | name,color,brand
+2. action performed by or performed on it: walk, eat,sleep  | drive, repair, fuel filling i.e wo kuchh kam kar sakega ya uspe kam kiya ja sakega.
+
+
+class: man
+property: full name,weight, height etc.
+action: walk,eat,sleep
+
+
+
+vishwajeet(man)
+Harsh(man)
+saurabh(man)
+
+
+i.e class is grouping of object
+
+viswjeet is a object of man class
+elon musk is a object of man class
+saurabh is a object of man class
+
+
+duniya m har chij ko class object se define kiya ja sakta hai...
+
+
+koi v object ek ya ek se jade class ka object ho sakta h..
+
+
+
+inheritance means dusre class ka property accuire karna.
+
+i.e software engineer class inherit propery of man.
+
+
+child: jo dusre class se property accuire karega.
+parent: jo dusre class ko property inherit karega.
+
+man: parent class
+software engineer: child class
+*/
