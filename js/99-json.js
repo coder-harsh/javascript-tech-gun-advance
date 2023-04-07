5232cc1eeb29a1494508bc579e98b915b683b49c
Harsh <harshj7.net@gmail.com>
2023-04-07 10:45:53 +0530

    add all 99-json till line no 42 and 99-data.json till line 16

---
+++ js/99-json.js
@@ -0,0 +1,45 @@
+/*
+ajax aur fetch api sikhne se pahle json sikhna jaruri hain
+json:javascript object notation
+
+json is text format for storing and transporting data.
+
+used in api and config files..
+
+config file of vscode is in json
+
+data is in name value pair
+data is seprated by comma
+
+curly braces hold objects.
+square brackets hold arrays
+square bracket hold arrays.
+
+
+{//object
+    "Name":"Harsh Kumar",
+    "Age":28,
+    "Language":["C","C++","Java"]
+}
+
+
+
+json is  laguages independent.
+
+code for reading and generating json exits in many programming languages.
+
+
+json se pahle xml ka use kiya jata tha json ke kam k liye.
+
+
+but
+json is faster in term of data parsing
+json is clear than xml
+json is lightweight than xml
+easy to read and write
+
+*/
+//now how to create json file
+
+
+
